7d53c769292df65d20f8e2ff7841173ab8f92427
AaronCoorahua <83974253+AaronCoorahua@users.noreply.github.com>
2024-05-13 10:26:01 -0500

    Update app-swagger.py

--- app-swagger.py
+++ app-swagger.py
@@ -24,6 +24,7 @@ def db_connection():
 
 # Model definition
 student_model = api.model('Student', {
+    'id': fields.Integer(readOnly=True, description='The unique identifier of a student'),
     'firstname': fields.String(required=True, description='First name of the student'),
     'lastname': fields.String(required=True, description='Last name of the student'),
     'gender': fields.String(required=True, description='Gender of the student'),
@@ -54,7 +55,6 @@ class StudentList(Resource):
         sql = """INSERT INTO students (firstname, lastname, gender, age)
                  VALUES (?, ?, ?, ?)"""
         cursor.execute(sql, (student['firstname'], student['lastname'], student['gender'], student['age']))
-        print("Last row id:", cursor.lastrowid)
         conn.commit()
         return student, 201
 
